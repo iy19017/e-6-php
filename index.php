2bef62c4eab40b6182cd8cccd0d99707e7d2f97c
iy19017 <50654720+iy19017@users.noreply.github.com>
2022-07-14 15:05:41 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  $pdo = new PDO ("pgsql:host=[host];dbname=[Database];user=[User];
-                   password=[Password]");
+  $pdo = new PDO ("pgsql:host=ec2-99-80-170-190.eu-west-1.compute.amazonaws.com;dbname=d4um8kn4vvubvj;user=ogqdkdyymaudqr;
+                   password=875862ff4cc3abd55689b3d3ae76f246e952ea4dda975c5a0108ac8f18676901");
   var_dump ($pdo->getAttribute (PDO::ATTR_SERVER_VERSION));
 ?>
